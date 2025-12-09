05231a804c1031802e90090addd1cc3ad8ec613c
Farand Diy Dat Mahazalfaa <68981409+cerealtwist@users.noreply.github.com>
2025-12-09 16:24:04 +0700

    Implement DBConnection
    Singleton

---
+++ backend/src/com/teralis/dao/DBConnection.java
@@ -0,0 +1,23 @@
+package com.teralis.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static Connection conn;
+    private static final String URL = "jdbc:mysql://localhost:3306/teralis?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = "pboK8";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            if (conn == null || conn.isClosed()){
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection(URL, USER, PASS);
+            }
+            return conn;
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver not found", e);
+        }
+    }
+}
