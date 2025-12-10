1e07a4a4c7c2f765986fdad61fd55b771652f67c
Farand Diy Dat Mahazalfaa <68981409+cerealtwist@users.noreply.github.com>
2025-12-10 16:03:35 +0700

    Update: PasswordHash
    change the label of password to passwordHash (for later use with PasswordHash utils)

--- backend/src/com/teralis/model/Admin.java
+++ backend/src/com/teralis/model/Admin.java
@@ -5,7 +5,7 @@ public class Admin extends User {
         this.role = "admin";
     }
 
-    public Admin(int id, String name, String email, String password) {
-        super(id, name, email, password, "admin");
+    public Admin(int id, String name, String email, String passwordHash) {
+        super(id, name, email, passwordHash, "admin");
     }
 }
