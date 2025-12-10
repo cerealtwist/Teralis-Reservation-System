63553f40b726e2da3dbe4ac96161d900cdee4cf1
Farand Diy Dat Mahazalfaa <68981409+cerealtwist@users.noreply.github.com>
2025-12-10 10:12:58 +0700

    Update: UserDAO.java

--- backend/src/com/teralis/dao/UserDAO.java
+++ backend/src/com/teralis/dao/UserDAO.java
@@ -4,19 +4,84 @@ import com.teralis.model.*;
 import java.sql.*;
 
 public class UserDAO {
-    public User findByEmail(string email){
-        return 0;
+    public User findByEmail(String email){
+        String sql = "SELECT * FROM users WHERE email = ?";
+
+        try(Connection conn = DBConnection.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql)) {
+                statement.setString(1, email);
+                ResultSet rs = statement.executeQuery();
+
+                if(rs.next()){
+                    return mapToUser(rs);
+                }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     public User getById(int id){
-        return 0;
+        String sql = "SELECT * FROM users Where id = ?";
+        try (Connection conn = DBConnection.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql)){
+                statement.setInt(1, id);
+                ResultSet rs = statement.executeQuery();
+
+                if(rs.next()){
+                    return mapToUser(rs);
+                }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     public boolean createUser(User u){
-        return 0;
+        String sql = "INSERT INTO users(name, email, password, role) VALUES (?, ?, ?, ?)";
+        try (Connection conn = DBConnection.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql)){
+                statement.setString(1, u.getName());
+                statement.setString(2, u.getEmail());
+                statement.setString(3, u.getPassword());
+                statement.setString(4, u.getRole());
+                
+                return statement.executeUpdate() > 0;
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return false;
     }
 
     private User mapToUser(ResultSet rs) throws SQLException{
-        return 0;
+        String role = rs.getString("role");
+        User u = null;
+
+        switch (role) {
+            case "student":
+                u = new Student();
+                break;
+            case "lecturer":
+                //u = new Lecturer(); <- NOT IMPLEMENTED
+                break;
+            case "admin":
+                //u = new Admin(); <- NOT IMPLEMENTED
+                break;
+            default:
+                throw new IllegalStateException("Unknown user role: " + role);
+        }
+
+        u.setId(rs.getInt("id"));
+        u.setName(rs.getString("name"));
+        u.setEmail(rs.getString("email"));
+        u.setPassword(rs.getString("password"));
+        u.setRole(role);
+
+        return u;
     }
 }
