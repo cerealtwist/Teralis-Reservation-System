9018370b6503bd0d29947417778bc8f3220de3d0
Farand Diy Dat Mahazalfaa <68981409+cerealtwist@users.noreply.github.com>
2025-12-10 09:52:46 +0700

    Create: UserDAO.java

---
+++ backend/src/com/teralis/dao/UserDAO.java
@@ -0,0 +1,22 @@
+package com.teralis.dao;
+
+import com.teralis.model.*;
+import java.sql.*;
+
+public class UserDAO {
+    public User findByEmail(string email){
+        return 0;
+    }
+
+    public User getById(int id){
+        return 0;
+    }
+
+    public boolean createUser(User u){
+        return 0;
+    }
+
+    private User mapToUser(ResultSet rs) throws SQLException{
+        return 0;
+    }
+}
