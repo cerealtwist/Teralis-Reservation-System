a71e2812248f8d7058061f0520f8a97056852206
Farand Diy Dat Mahazalfaa <68981409+cerealtwist@users.noreply.github.com>
2025-12-10 16:08:13 +0700

    Update AuthController.java

--- backend/src/com/teralis/controller/AuthController.java
+++ backend/src/com/teralis/controller/AuthController.java
@@ -1,5 +1,18 @@
 package com.teralis.controller;
 
-public class AuthController {
+import com.teralis.dao.UserDAO;
+import com.teralis.model.User;
+import com.teralis.utils.JsonResponse;
+import com.teralis.utils.PasswordHash;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+
+public class AuthController extends HttpServlet {
+
+    private final UserDAO userDAO = new UserDAO();
     
 }
