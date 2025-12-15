e2ed7e0680813489559cdd79adecd14f978cf0fe
Farand Diy Dat Mahazalfaa <68981409+cerealtwist@users.noreply.github.com>
2025-12-15 13:50:48 +0700

    Implement frontend (login.html) + auth.js

---
+++ frontend/js/auth.js
@@ -0,0 +1,45 @@
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  fetch("http://localhost:8080/WebContent/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    credentials: "include",
+    body: JSON.stringify({ email, password })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.status === "success") {
+      fetchProfile();
+    } else {
+      document.getElementById("error").innerText = data.message;
+    }
+  })
+  .catch(err => {
+    document.getElementById("error").innerText = "Server error";
+  });
+}
+
+function fetchProfile() {
+  fetch("http://localhost:8080/WebContent/api/users/me", {
+    method: "GET",
+    credentials: "include"
+  })
+  .then(res => res.json())
+  .then(user => {
+    redirectByRole(user.role);
+  });
+}
+
+function redirectByRole(role) {
+  if (role === "admin") {
+    window.location.href = "admin.html";
+  } else if (role === "student") {
+    // blm buat file
+  } else if (role === "lecturer") {
+    // blm buat file
+  }
+}
